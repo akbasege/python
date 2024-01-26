6be8da52648dee6780c5542a2b24ca00dcf3721b
akbasege <71585777+akbasege@users.noreply.github.com>
2024-01-26 23:23:16 +0300

    Update ols_statsmodels.py

--- ols_statsmodels.py
+++ ols_statsmodels.py
@@ -1,2 +1,15 @@
 import pandas as pd
 import statsmodels.api as sm
+
+xls= pd.ExcelFile('abc.xlsx')
+
+df1=pd.read_excel(xls, 'xyz')
+
+y=df.iloc[:,3:]
+X=df.iloc[:,:3]
+
+X=sm.add_constant(X)
+
+fit=sm.OLS(y,X).fit()
+
+fit.summary()
