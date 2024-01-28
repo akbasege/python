bedb628162e88ac2a6c34a245591bd0610342a93
akbasege <71585777+akbasege@users.noreply.github.com>
2024-01-28 20:20:39 +0300

    Update ols_sklearn.py

--- ols_sklearn.py
+++ ols_sklearn.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-xls= pd.ExcelFile('abc.xlsx')
 
-df1=pd.read_excel(xls, 'xyz')
 
-y=df.iloc[:,3:]
-X=df.iloc[:,:3]
+df=pd.read_csv('inflation interest unemployment.csv')
+
+y=df['Unemployment, total (% of total labor force) (modeled ILO estimate)']
+X=df['incomeLevel','Real interest rate (%)','Inflation, consumer prices (annual %)','adminregion']
 
 reg=LinearRegression(fit_intercept=True).fit(X,y)
 
